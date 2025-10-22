9e1421883e8742ccda7eaf672fee4ccad91fd5b6
HyoscyamusA <110112047+HyoscyamusA@users.noreply.github.com>
2025-10-22 11:26:29 +0800

    Add files via upload
    c# 作业 记事本

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TextEditor
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+
+            // menuStrip1
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                this.fileToolStripMenuItem,
+                this.editToolStripMenuItem,
+                this.formatToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+
+            // 文件菜单
+            this.fileToolStripMenuItem.Text = "文件";
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                this.openToolStripMenuItem,
+                this.saveToolStripMenuItem,
+                this.exitToolStripMenuItem});
+
+            this.openToolStripMenuItem.Text = "打开";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+
+            this.saveToolStripMenuItem.Text = "保存";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+
+            this.exitToolStripMenuItem.Text = "退出";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+
+            // 编辑菜单
+            this.editToolStripMenuItem.Text = "编辑";
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                this.findToolStripMenuItem});
+
+            this.findToolStripMenuItem.Text = "查找";
+            this.findToolStripMenuItem.Click += new System.EventHandler(this.findToolStripMenuItem_Click);
+
+            // 格式菜单
+            this.formatToolStripMenuItem.Text = "格式";
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                this.fontToolStripMenuItem,
+                this.colorToolStripMenuItem});
+
+            this.fontToolStripMenuItem.Text = "字体";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+
+            this.colorToolStripMenuItem.Text = "颜色";
+            this.colorToolStripMenuItem.Click += new System.EventHandler(this.colorToolStripMenuItem_Click);
+
+            // richTextBox1
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 390);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+
+            // statusStrip1
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 420);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 30);
+            this.statusStrip1.TabIndex = 2;
+
+            // toolStripStatusLabel1
+            this.toolStripStatusLabel1.Text = "就绪";
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Text = "简易文本编辑器";
+
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic; // 用于 InputBox 查找功能
+
+namespace TextEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 打开文件
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "文本文件|*.txt|所有文件|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(ofd.FileName);
+                toolStripStatusLabel1.Text = "已打开：" + ofd.FileName;
+            }
+        }
+
+        // 保存文件
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "文本文件|*.txt";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, richTextBox1.Text);
+                toolStripStatusLabel1.Text = "已保存：" + sfd.FileName;
+            }
+        }
+
+        // 设置字体
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Font = fontDialog.Font;
+            }
+        }
+
+        // 设置颜色
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.ForeColor = colorDialog.Color;
+            }
+        }
+
+        // 查找文字
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (Form inputForm = new Form())
+            {
+                inputForm.Text = "查找";
+                Label label = new Label() { Left = 10, Top = 20, Text = "请输入要查找的文字：" };
+                TextBox textBox = new TextBox() { Left = 10, Top = 50, Width = 200 };
+                Button okButton = new Button() { Text = "确定", Left = 80, Width = 80, Top = 80, DialogResult = DialogResult.OK };
+
+                inputForm.Controls.Add(label);
+                inputForm.Controls.Add(textBox);
+                inputForm.Controls.Add(okButton);
+                inputForm.AcceptButton = okButton;
+
+                if (inputForm.ShowDialog() == DialogResult.OK)
+                {
+                    string findText = textBox.Text;
+                    if (!string.IsNullOrEmpty(findText))
+                    {
+                        int index = richTextBox1.Find(findText);
+                        if (index >= 0)
+                        {
+                            richTextBox1.Select(index, findText.Length);
+                            richTextBox1.Focus();
+                        }
+                        else
+                        {
+                            MessageBox.Show("未找到指定内容！");
+                        }
+                    }
+                }
+            }
+        }
+
+
+        // 退出
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
